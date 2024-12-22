272a73e5d21a5cc2329c6cce710f0e2f465a161f
millanadhikari <millanadhikari@gmail.com>
2024-12-22 11:24:01 +1100

    homepage done with animation

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,38 @@
+import Button from "@/components/Button";
+
+export default function Hero() {
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-4 md:flex-1 w-full"
+                    />
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="whitespace-nowrap"
+                        size="sm"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
+}
